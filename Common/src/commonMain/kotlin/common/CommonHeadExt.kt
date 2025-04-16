28f409616d217c03850bfc8adc848d3300341147
umeshsolanki <umesh.solanki@go-mmt.com>
2025-04-16 22:17:02 +0530

    Running with 2.1.20

--- Common/src/commonMain/kotlin/common/CommonHeadExt.kt
+++ Common/src/commonMain/kotlin/common/CommonHeadExt.kt
@@ -6,10 +6,13 @@ import kotlinx.html.link
 import kotlinx.html.script
 
 inline fun HEAD.addBootstrapAndCommon(crossinline content: HEAD.() -> Unit) {
-    link(rel = "stylesheet", href = "/css/bootstrap.min.css", type = "text/css")
+    link(rel = "stylesheet", href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css", type = "text/css")
+    script(src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js", crossorigin = ScriptCrossorigin.anonymous) {}
+
     link(rel = "stylesheet", href = "/styles.css", type = "text/css")
-    script(src = "/js/bootstrap.bundle.min.js", crossorigin = ScriptCrossorigin.anonymous) {}
     script(src = "/js/Common.js") {}
+//    script(src = "/js/bootstrap.bundle.min.js", crossorigin = ScriptCrossorigin.anonymous) {}
+//    link(rel = "stylesheet", href = "/css/bootstrap.min.css", type = "text/css")
     link(
         rel = "stylesheet",
         href = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css",
